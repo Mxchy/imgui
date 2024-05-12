700e91d0b4e0f3115cae6a5b20e8a88a59ed422d
Mxchy <michael.s.retter@gmail.com>
2024-05-12 15:31:26 +0200

    Fixed premmake file

--- premake5.lua
+++ premake5.lua
@@ -8,15 +8,15 @@ project "ImGui"
 
 	files
 	{
-    "imconfig.h"
-    "imgui.h"
-    "imgui.cpp"
-    "imgui_draw.cpp"
-    "imgui_internal.h"
-    "imgui_widgets.cpp"
-    "imstb_rectpack.h"
-    "imstb_textedit.h"
-    "imstb_truetype.h"
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
     "imgui_demo.cpp"
 	}
 	includedirs
